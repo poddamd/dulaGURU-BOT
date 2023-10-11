a8c0d1d0a85e45f82bd5fd70294ec8e8e34712c6
poddamd <147582259+poddamd@users.noreply.github.com>
2023-10-11 14:57:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94771360470', 'me', true],
+  ['94741313454', 'podda', true], 
   [''] 
 ] //Number of owners
 
